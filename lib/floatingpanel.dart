871e048ebb6fc48e48198ff8f7e08714231dd27e
hamed <hamed.hp332@gmail.com>
2021-07-23 22:10:05 +0430

    split everything (why?!)

--- lib/floatingpanel.dart
+++ lib/floatingpanel.dart
@@ -11,6 +11,8 @@ class FloatBoxPanel extends StatefulWidget {
   final double positionLeft;
   final Color borderColor;
   final double borderWidth;
+
+  /// Widget size if the width of the panel;
   final double size;
   final double iconSize;
   final IconData panelIcon;
@@ -18,11 +20,14 @@ class FloatBoxPanel extends StatefulWidget {
   final Color backgroundColor;
   final Color contentColor;
   final PanelShape panelShape;
-  final PanelState panelState;
+
+  // final PanelState panelState;
   final double panelOpenOffset;
   final int panelAnimDuration;
   final Curve panelAnimCurve;
   final DockType dockType;
+
+  /// Dock offset creates the boundary for the page depending on the DockType;
   final double dockOffset;
   final int dockAnimDuration;
   final Curve dockAnimCurve;
@@ -39,7 +44,7 @@ class FloatBoxPanel extends StatefulWidget {
     this.panelIcon = Icons.add,
     this.size = 70,
     BorderRadius? borderRadius,
-    this.panelState = PanelState.closed,
+    // this.panelState = PanelState.closed,
     this.panelOpenOffset = 30.0,
     this.panelAnimDuration = 600,
     this.panelAnimCurve = Curves.fastLinearToSlowEaseIn,
@@ -76,6 +81,10 @@ class _FloatBoxState extends State<FloatBoxPanel> {
   // speed than when the panel is being dragged;
   int _movementSpeed = 0;
 
+  // Width and height of page is required for the dragging the panel;
+  double get _pageWidth => MediaQuery.of(context).size.width;
+  double get _pageHeight => MediaQuery.of(context).size.height;
+
   @override
   void initState() {
     _positionTop = widget.positionTop;
@@ -84,117 +93,113 @@ class _FloatBoxState extends State<FloatBoxPanel> {
     super.initState();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // Width and height of page is required for the dragging the panel;
-    double _pageWidth = MediaQuery.of(context).size.width;
-    double _pageHeight = MediaQuery.of(context).size.height;
+  //#region Methods
 
-    // All Buttons;
-    List<IconData> _buttons = widget.buttons;
-
-    // Dock offset creates the boundary for the page depending on the DockType;
-    double _dockOffset = widget.dockOffset;
-
-    // Widget size if the width of the panel;
-    double _widgetSize = widget.size;
-
-    // **** METHODS ****
-
-    // Dock boundary is calculated according to the dock offset and dock type.
-    double _dockBoundary() {
-      if (widget.dockType == DockType.inside) {
-        // If it's an 'inside' type dock, dock offset will remain the same;
-        return _dockOffset;
-      }
-
-      // If it's an 'outside' type dock, dock offset will be inverted, hence
-      // negative value;
-      return -_dockOffset;
+  // Dock boundary is calculated according to the dock offset and dock type.
+  double _dockBoundary() {
+    if (widget.dockType == DockType.inside) {
+      // If it's an 'inside' type dock, dock offset will remain the same;
+      return widget.dockOffset;
     }
 
-    // If panel shape is set to rectangle, the border radius will be set to custom
-    // border radius property of the WIDGET, else it will be set to the size of
-    // widget to make all corners rounded.
-    BorderRadius _borderRadius() {
-      if (widget.panelShape == PanelShape.rectangle) {
-        // If panel shape is 'rectangle', border radius can be set to custom or 0;
-        return widget.borderRadius;
-      }
+    // If it's an 'outside' type dock, dock offset will be inverted, hence
+    // negative value;
+    return -(widget.dockOffset);
+  }
 
-      // If panel shape is 'rounded', border radius will be the size of widget
-      // to make it rounded;
-      return BorderRadius.circular(_widgetSize);
+  // If panel shape is set to rectangle, the border radius will be set to custom
+  // border radius property of the WIDGET, else it will be set to the size of
+  // widget to make all corners rounded.
+  BorderRadius _borderRadius() {
+    if (widget.panelShape == PanelShape.rectangle) {
+      // If panel shape is 'rectangle', border radius can be set to custom or 0;
+      return widget.borderRadius;
     }
 
-    // Total buttons are required to calculate the height of the panel;
-    double _totalButtons() {
-      return widget.buttons.length.toDouble();
+    // If panel shape is 'rounded', border radius will be the size of widget
+    // to make it rounded;
+    return BorderRadius.circular(widget.size);
+  }
+
+  // Height of the panel according to the panel state;
+  double _panelHeight() {
+    if (_panelState == PanelState.open) {
+      final buttonLength = widget.buttons.length;
+
+      // Panel height will be in multiple of total buttons, I have added "1"
+      // digit height for each button to fix the overflow issue. Don't know
+      // what's causing this, but adding "1" fixed the problem for now.
+      return (widget.size + (widget.size + 1) * buttonLength) +
+          (widget.borderWidth);
     }
 
-    // Height of the panel according to the panel state;
-    double _panelHeight() {
-      if (_panelState == PanelState.open) {
-        // Panel height will be in multiple of total buttons, I have added "1"
-        // digit height for each button to fix the overflow issue. Don't know
-        // what's causing this, but adding "1" fixed the problem for now.
-        return (_widgetSize + (_widgetSize + 1) * _totalButtons()) +
-            (widget.borderWidth);
-      }
+    return widget.size + (widget.borderWidth) * 2;
+  }
 
-      return _widgetSize + (widget.borderWidth) * 2;
+  // Panel top needs to be recalculated while opening the panel, to make sure
+  // the height doesn't exceed the bottom of the page;
+  void _calcPanelTop() {
+    if (_positionTop + _panelHeight() > _pageHeight + _dockBoundary()) {
+      _positionTop = _pageHeight - _panelHeight() + _dockBoundary();
+    }
+  }
+
+  // Dock Left position when open;
+  double _openDockLeft() {
+    if (_positionLeft < (_pageWidth / 2)) {
+      // If panel is docked to the left;
+      return widget.panelOpenOffset;
     }
 
-    // Panel top needs to be recalculated while opening the panel, to make sure
-    // the height doesn't exceed the bottom of the page;
-    void _calcPanelTop() {
-      if (_positionTop + _panelHeight() > _pageHeight + _dockBoundary()) {
-        _positionTop = _pageHeight - _panelHeight() + _dockBoundary();
-      }
-    }
+    // If panel is docked to the right;
+    return ((_pageWidth - widget.size)) - (widget.panelOpenOffset);
+  }
 
-    // Dock Left position when open;
-    double _openDockLeft() {
-      if (_positionLeft < (_pageWidth / 2)) {
-        // If panel is docked to the left;
-        return widget.panelOpenOffset;
-      }
-
-      // If panel is docked to the right;
-      return ((_pageWidth - _widgetSize)) - (widget.panelOpenOffset);
-    }
-
-    // Panel border is only enabled if the border width is greater than 0;
-    Border? _panelBorder() {
-      if (widget.borderWidth > 0) {
-        return Border.all(
-          color: widget.borderColor,
-          width: widget.borderWidth,
-        );
-      }
-
+  // Panel border is only enabled if the border width is greater than 0;
+  Border? _panelBorder() {
+    if (widget.borderWidth <= 0) {
       return null;
     }
 
-    // Force dock will dock the panel to it's nearest edge of the screen;
-    void _forceDock() {
-      // Calculate the center of the panel;
-      double center = _positionLeft + (_widgetSize / 2);
+    return Border.all(
+      color: widget.borderColor,
+      width: widget.borderWidth,
+    );
+  }
 
-      // Set movement speed to the custom duration property or '300' default;
-      _movementSpeed = widget.dockAnimDuration;
+  // Force dock will dock the panel to it's nearest edge of the screen;
+  void _forceDock() {
+    // Calculate the center of the panel;
+    double center = _positionLeft + (widget.size / 2);
 
-      // Check if the position of center of the panel is less than half of the
-      // page;
-      if (center < _pageWidth / 2) {
-        // Dock to the left edge;
-        _positionLeft = 0.0 + _dockBoundary();
-      } else {
-        // Dock to the right edge;
-        _positionLeft = (_pageWidth - _widgetSize) - _dockBoundary();
-      }
-    }
+    // Set movement speed to the custom duration property or '300' default;
+    _movementSpeed = widget.dockAnimDuration;
 
+    // Check if the position of center of the panel is less than half of the
+    // page;
+    final offsetOfLeftEdge = (center < _pageWidth / 2)
+        // Dock to the left edge
+        ? 0.0
+        // Dock to the right edge
+        : (_pageWidth - widget.size);
+
+    _positionLeft = offsetOfLeftEdge - _dockBoundary();
+  }
+
+  //#endregion
+
+  @override
+  Widget build(BuildContext context) {
+    return _animatedPositioned(
+      child: _animatedContainer(
+        child: _panel(),
+      ),
+    );
+  }
+
+  //#region panel body
+
+  Widget _animatedPositioned({required Widget child}) {
     // Animated positioned widget can be moved to any part of the screen with
     // animation;
     return AnimatedPositioned(
@@ -202,138 +207,157 @@ class _FloatBoxState extends State<FloatBoxPanel> {
       top: _positionTop,
       left: _positionLeft,
       curve: widget.dockAnimCurve,
+      child: child,
+    );
+  }
 
-      // Animated Container is used for easier animation of container height;
-      child: AnimatedContainer(
-        duration: Duration(milliseconds: widget.panelAnimDuration),
-        width: _widgetSize,
-        height: _panelHeight(),
-        decoration: BoxDecoration(
-          color: widget.backgroundColor,
-          borderRadius: _borderRadius(),
-          border: _panelBorder(),
-        ),
-        curve: widget.panelAnimCurve,
-        child: Wrap(
-          direction: Axis.horizontal,
-          children: [
-            // Gesture detector is required to detect the tap and drag on the panel;
-            GestureDetector(
-              onPanEnd: (event) {
-                setState(
-                  () {
-                    _forceDock();
-                  },
-                );
-              },
-              onPanStart: (event) {
-                // Detect the offset between the top and left side of the panel and
-                // x and y position of the touch(click) event;
-                _panOffsetTop = event.globalPosition.dy - _positionTop;
-                _panOffsetLeft = event.globalPosition.dx - _positionLeft;
-              },
-              onPanUpdate: (event) {
-                setState(
-                  () {
-                    // Close Panel if opened;
-                    _panelState = PanelState.closed;
+  Widget _animatedContainer({required Widget child}) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: widget.panelAnimDuration),
+      width: widget.size,
+      height: _panelHeight(),
+      decoration: BoxDecoration(
+        color: widget.backgroundColor,
+        borderRadius: _borderRadius(),
+        border: _panelBorder(),
+      ),
+      curve: widget.panelAnimCurve,
+      child: child,
+    );
+  }
 
-                    // Reset Movement Speed;
-                    _movementSpeed = 0;
+  Widget _panel() {
+    return Wrap(
+      direction: Axis.horizontal,
+      children: [
+        _gestureDetector(),
+        _buttons(),
+      ],
+    );
+  }
 
-                    // Calculate the top position of the panel according to pan;
-                    _positionTop = event.globalPosition.dy - _panOffsetTop;
+  Widget _gestureDetector() {
+    // Gesture detector is required to detect the tap and drag on the panel;
+    return GestureDetector(
+      onPanEnd: (event) {
+        setState(_forceDock);
+      },
+      onPanStart: (event) {
+        // Detect the offset between the top and left side of the panel and
+        // x and y position of the touch(click) event;
+        _panOffsetTop = event.globalPosition.dy - _positionTop;
+        _panOffsetLeft = event.globalPosition.dx - _positionLeft;
+      },
+      onPanUpdate: onPanUpdateGesture,
+      onTap: _onTapGesture,
+      child: _FloatButton(
+        size: widget.size,
+        icon: widget.panelIcon,
+        color: widget.contentColor,
+        iconSize: widget.iconSize,
+      ),
+    );
+  }
 
-                    // Check if the top position is exceeding the dock boundaries;
-                    if (_positionTop < 0 + _dockBoundary()) {
-                      _positionTop = 0 + _dockBoundary();
-                    }
-                    if (_positionTop >
-                        (_pageHeight - _panelHeight()) - _dockBoundary()) {
-                      _positionTop =
-                          (_pageHeight - _panelHeight()) - _dockBoundary();
-                    }
+  //#region Gesture functions
 
-                    // Calculate the Left position of the panel according to pan;
-                    _positionLeft = event.globalPosition.dx - _panOffsetLeft;
+  void onPanUpdateGesture(event) {
+    setState(
+      () {
+        // Close Panel if opened;
+        _panelState = PanelState.closed;
 
-                    // Check if the left position is exceeding the dock boundaries;
-                    if (_positionLeft < 0 + _dockBoundary()) {
-                      _positionLeft = 0 + _dockBoundary();
-                    }
-                    if (_positionLeft >
-                        (_pageWidth - _widgetSize) - _dockBoundary()) {
-                      _positionLeft =
-                          (_pageWidth - _widgetSize) - _dockBoundary();
-                    }
-                  },
-                );
-              },
-              onTap: () {
-                setState(
-                  () {
-                    // Set the animation speed to custom duration;
-                    _movementSpeed = widget.panelAnimDuration;
+        // Reset Movement Speed;
+        _movementSpeed = 0;
 
-                    if (_panelState == PanelState.open) {
-                      // If panel state is "open", set it to "closed";
-                      _panelState = PanelState.closed;
+        // Calculate the top position of the panel according to pan;
+        _positionTop = event.globalPosition.dy - _panOffsetTop;
 
-                      // Reset panel position, dock it to nearest edge;
-                      _forceDock();
+        // Check if the top position is exceeding the dock boundaries;
+        if (_positionTop < 0 + _dockBoundary()) {
+          _positionTop = 0 + _dockBoundary();
+        }
+        if (_positionTop > (_pageHeight - _panelHeight()) - _dockBoundary()) {
+          _positionTop = (_pageHeight - _panelHeight()) - _dockBoundary();
+        }
 
-                      print("Float panel closed.");
-                    } else {
-                      // If panel state is "closed", set it to "open";
-                      _panelState = PanelState.open;
+        // Calculate the Left position of the panel according to pan;
+        _positionLeft = event.globalPosition.dx - _panOffsetLeft;
 
-                      // Set the left side position;
-                      _positionLeft = _openDockLeft();
+        // Check if the left position is exceeding the dock boundaries;
+        if (_positionLeft < 0 + _dockBoundary()) {
+          _positionLeft = 0 + _dockBoundary();
+        }
+        if (_positionLeft > (_pageWidth - widget.size) - _dockBoundary()) {
+          _positionLeft = (_pageWidth - widget.size) - _dockBoundary();
+        }
+      },
+    );
+  }
 
-                      _calcPanelTop();
+  void _onTapGesture() {
+    setState(
+      () {
+        // Set the animation speed to custom duration;
+        _movementSpeed = widget.panelAnimDuration;
 
-                      print("Float Panel Open.");
-                    }
-                  },
-                );
-              },
-              child: _FloatButton(
-                size: widget.size,
-                icon: widget.panelIcon,
-                color: widget.contentColor,
-                iconSize: widget.iconSize,
-              ),
-            ),
-            Visibility(
-              visible: true, // TODO:: _contentVisibility,
-              child: Container(
-                child: Column(
-                  children: List.generate(
-                    _buttons.length,
-                    (index) {
-                      return GestureDetector(
-                        onTap: () {
-                          if (widget.onPressed != null) {
-                            widget.onPressed!(index);
-                          }
-                        },
-                        child: _FloatButton(
-                          size: widget.size,
-                          icon: _buttons[index],
-                          color: widget.contentColor,
-                          iconSize: widget.iconSize,
-                        ),
-                      );
-                    },
-                  ),
+        if (_panelState == PanelState.open) {
+          // If panel state is "open", set it to "closed";
+          _panelState = PanelState.closed;
+
+          // Reset panel position, dock it to nearest edge;
+          _forceDock();
+
+          print("Float panel closed.");
+        } else {
+          // If panel state is "closed", set it to "open";
+          _panelState = PanelState.open;
+
+          // Set the left side position;
+          _positionLeft = _openDockLeft();
+
+          _calcPanelTop();
+
+          print("Float Panel Open.");
+        }
+      },
+    );
+  }
+
+  //#endregion
+
+  Widget _buttons() {
+    final buttons = widget.buttons;
+
+    return Visibility(
+      visible: true, // TODO:: _contentVisibility,
+      child: Container(
+        child: Column(
+          children: List.generate(
+            buttons.length,
+            (index) {
+              return GestureDetector(
+                onTap: () {
+                  if (widget.onPressed != null) {
+                    widget.onPressed!(index);
+                  }
+                },
+                child: _FloatButton(
+                  size: widget.size,
+                  icon: buttons[index],
+                  color: widget.contentColor,
+                  iconSize: widget.iconSize,
                 ),
-              ),
-            ),
-          ],
+              );
+            },
+          ),
         ),
       ),
     );
   }
+
+  //#endregion
+
 }
 
 class _FloatButton extends StatelessWidget {
